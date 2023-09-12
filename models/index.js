ad3355974c7cba6561ce742663c207a6e7bd281b
Toussaint Gilbert <toussaintdgilbert@gmail.com>
2023-09-12 18:56:07 -0500

    added database and removed unnecessary code

--- models/index.js
+++ models/index.js
@@ -1,12 +1,7 @@
 const User = require('./User');
 const Trips = require('./Trips');
 
-Trips.hasMany(location, {
-  foreignKey: 'trips_id',
-});
 
-location.belongsTo(User, {
-  foreignKey: 'trips_id',
-});
+
 
 module.exports = { User, Trips};
